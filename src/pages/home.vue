bb260fe2c0becf532418812bb8ad64b4b09d6937
yimocoding <1280160831@qq.com>
2017-03-19 18:51:38 +0800

    添加统计代码

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,7 +5,7 @@
             <li @click="$options.filters.openRedirect('http://coding.net/u/yimocoding/p/metools/git/blob/master/src/pages/home.vue')">查看代码</li>
         </ul>
         <div style="width:100%;height:100%;">
-            <div v-for="item in menus" class="welcome layui-circle" :style="color()">
+            <div v-for="item in menus" class="welcome layui-circle" @click="$options.filters.openRoute(item.MenuUrl)" :style="color()">
                 <router-link :to="{path:item.MenuUrl}">{{item.MenuName}}</router-link>
             </div>
         </div>
